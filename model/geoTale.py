769b4d01ecf6aa9caaf569a5c33ee2ce082af4b4
ythuang13 <ythuang13@users.noreply.github.com>
2021-03-16 23:38:55 -0700

    Start writing the model of GeoTale

--- model/geoTale.py
+++ model/geoTale.py
@@ -1,3 +1,19 @@
+from geopy.geocoders import Nominatim
+
+
 class GeoTale:
     def __init__(self):
+        self.geolocator = Nominatim(user_agent="GeoTale")
+
+    def add_story(self):
         pass
+
+    def hear_story(self, location):
+        location = self.geolocator.geocode(location)
+        print(location.latitude, location.longitude)
+
+
+if __name__ == "__main__":
+    # test for GeoTale
+    gt = GeoTale()
+    gt.hear_story("92780")
